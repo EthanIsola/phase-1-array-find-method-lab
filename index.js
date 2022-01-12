678741da6037e22f9298487b95c554982eabe13a
Ethan Isola <isolaethan@gmail.com>
2022-01-12 07:11:18 -0600

    commit tree 752770c43b74e47d9d56d2bc8a6551f07202c494

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+    const winYear = record.find(function win(record){
+        return record.result === "W";
+    });
+    if(winYear != undefined){
+        return winYear.year;
+    }
+    else{
+        return undefined;
+    }
+}
